10b6013bd8d06426a24e28054fd70ce3ef4df8fe
Jack Hughes <jack@jackhughesweb.com>
2015-12-12 15:05:56 +0000

    Added changes

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-if(isset($_POST)){
-	echo "Ello!";
+if(isset($_POST["movie_id"])){
+	?>
+
+{
+	"movie_id": "1234567890"
+}
+
+	<?php
 }else{
 	include('templates/header.php');
 	include('templates/movies.php');
--- templates/movies.php
+++ templates/movies.php
@@ -2,9 +2,34 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<script src="script.js"></script>
+	<script src="jquery-1.11.3.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('.likebtn').click(function() {
+				console.log("btn clicked!");
+				$.post('/', {
+					"opinion": "like",
+					"movie_id": "123"
+				}, function (raw_data) {
+					var data = $.parseJSON(raw_data);
+					console.log(data['movie_id']);
+				});
+			});	
+		});
+	</script>
 	<title>Movies</title>
 </head>
 <body>
-	
+	<header>MovieTinder</header>
+	<section class="flex">
+		<div class="test">abcd</div>
+		<div class="test2">
+			<div class="testbtn likebtn">like</div>
+			<div class="testbtn">nw</div>
+			<div class="testbtn">dislike</div>
+		</div>
+	</section>
 </body>
 </html>
